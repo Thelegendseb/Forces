f70767364695ae705dae0ff42aeb701be9709345
Sebastian Clarke <SebClarke1@outlook.com>
2022-05-05 23:18:24 +0100

    Add project files.

---
+++ Entity.vb
@@ -0,0 +1,68 @@
+﻿Public Class Entity
+
+    Protected Position As Vector
+    Protected Velocity As Vector
+    Protected Acceleration As Vector
+
+    Sub New()
+        Me.Position = Vector.Zero
+        Me.Velocity = Vector.Zero
+        Me.Acceleration = Vector.Zero
+    End Sub
+
+    Public Sub Update(FormBounds As Rectangle)
+
+        BorderCollisionChecks(FormBounds)
+        Me.Velocity += Me.Acceleration
+        Me.Position += Me.Velocity
+
+    End Sub
+    Private Sub BorderCollisionChecks(FormBounds As Rectangle)
+        Dim CollisionOccured As Boolean = False
+        If Me.Position.X > FormBounds.X + FormBounds.Width Then
+            Me.Position.X = FormBounds.X + FormBounds.Width
+            Me.Velocity.X *= -1
+            CollisionOccured = True
+        End If
+        If Me.Position.X < FormBounds.X Then
+            Me.Position.X = FormBounds.X
+            Me.Velocity.X *= -1
+            CollisionOccured = True
+        End If
+        If Me.Position.Y > FormBounds.Y + FormBounds.Height Then
+            Me.Position.Y = FormBounds.Y + FormBounds.Height
+            Me.Velocity.Y *= -1
+            CollisionOccured = True
+        End If
+        If Me.Position.Y < FormBounds.Y Then
+            Me.Position.Y = FormBounds.Y
+            Me.Velocity.Y *= -1
+            CollisionOccured = True
+        End If
+
+        If CollisionOccured Then
+            Me.Velocity *= New Vector(0.8, 0.8) 'Friction/ Loss of Ek?
+        End If
+    End Sub
+
+    '======GETTERS/SETTERS/MATHS OPS========
+    Public Function GetPosition() As Vector
+        Return Me.Position
+    End Function
+    Public Sub SetPosition(ByVal Position As Vector)
+        Me.Position = Position
+    End Sub
+    Public Function GetVelocity() As Vector
+        Return Me.Velocity
+    End Function
+    Public Sub SetVelocity(ByVal Velocity As Vector)
+        Me.Velocity = Velocity
+    End Sub
+    Public Function GetAcceleration() As Vector
+        Return Me.Acceleration
+    End Function
+    Public Sub SetAcceleration(ByVal Acceleration As Vector)
+        Me.Acceleration = Acceleration
+    End Sub
+    '=============================
+End Class
---
+++ Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Dim WithEvents Time As New Timer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        EntityInit()
+
+        Time.Interval = 20  'Put into a session
+        Time.Start()
+    End Sub
+    Private Sub Time_Tick(sender As Object, e As EventArgs) Handles Time.Tick
+        TestEntity.Update(New Rectangle(0, 0, Me.ClientSize.Width, Me.ClientSize.Height))
+        DrawEntity(TestEntity)
+    End Sub
+    '=================================
+    Dim TestEntity As Entity
+
+    Sub EntityInit()
+        TestEntity = New Entity
+        TestEntity.SetPosition(New Vector(100, 150))
+        TestEntity.SetVelocity(New Vector(10, 0))
+        TestEntity.SetAcceleration(New Vector(0, 1))
+    End Sub
+    Sub DrawEntity(Entity As Entity)
+        Using g As Graphics = Me.CreateGraphics
+            g.Clear(Me.BackColor)
+            g.FillEllipse(Brushes.Black, CSng(Entity.GetPosition.X) - 10, CSng(Entity.GetPosition.Y) - 10, 20, 20)
+        End Using
+    End Sub
+    '=================================
+End Class
